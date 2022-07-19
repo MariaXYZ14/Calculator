831ec036caef8ba54248e56ed16933b73f715d6b
MariaXYZ14 <mariaxyz14@gmail.com>
2022-07-19 13:53:25 +0200

    versio10

--- calculator.js
+++ calculator.js
@@ -1,165 +1,205 @@
-var y=null;
-var z=null;
-var result;
-var commas=0;
-
+let first_number;
+let second_number;
+let operation='';
 
 window.onload = function(){ 
     pantalla=document.getElementById("textoPantalla"); 
     document.onkeydown = keyboard; }
 
-function returnValue(x) {
-  
-    if(x=='/' || x=='x' ||x=='-'|| x=='+'){
-        commas=0;
-    }
 
-    if(x==','  &&  document.getElementById('screen').value == 0){
-        
-        document.getElementById('screen').value+="0,";
-    }else{
-
-         document.getElementById('screen').value += x;
-
-    }
-
-    let first_number =document.getElementById('screen').value;
-    first_number=first_number.replace(',','.');
- 
-    const commaActive = document.getElementById('comma');
-    commaActive.disabled = false; 
-
-    if(first_number.includes('.')){
+function clickNumber(number){
    
-        commaActive.disabled = true; 
-        commas++;
+    let screen=document.getElementById('screen');
 
 
+    if(screen.value == 0){
+        
+        screen.value = number;
+
+    }
+    else if(calculateLength(screen.value)>=10){
+
+        return;
     }
     else{
 
-        commaActive.disabled = false; 
+        screen.value += number;
 
     }
+}
 
-    if(first_number.includes('+')){
-        y=parseFloat(first_number.substring(0,first_number.indexOf('+')));
-        commaActive.disabled = false
+function calculateLength(len){
 
-        let arr=Array.from(document.getElementById('screen').value);
-        commas=0;
+  let result=0;
 
-        for(i=0;i<first_number.length;i++){
-            if(arr[i]==','){ commas++;}
-               console.log(commas);
+  for(let i=0;i<len.length;i++){
 
-            if(commas>0){
-   
-               commaActive.disabled = true; 
-        
-               
-            }
-            else{
-        
-                commaActive.disabled = false; 
-        
-            }
-        }
-        
+    if(len[i]!=',' && len[i]!='-'){result++;}
+
+  }
+  return result;
+
+}
+
+function clickComma(){
     
-        
+    
+    let screen=document.getElementById('screen');
+
+    if(screen.value.includes(',')){
+        return;
     }
-    if(first_number.includes('-')){
-        y=parseFloat(first_number.substring(0,first_number.indexOf('-')));
-        commaActive.disabled = false
+    else if(calculateLength(screen.value)>=10) {
+        return;
 
-        let arr=Array.from(document.getElementById('screen').value);
-        commas=0;
+    }
+    else if(screen.value==''){
 
-        for(i=0;i<first_number.length;i++){
-            if(arr[i]==','){ commas++;}
-               console.log(commas);
+        screen.value="0,";
+    }
+    else{
+        screen.value+=",";
+    }
+}
 
-            if(commas>0){
-   
-               commaActive.disabled = true; 
-        
-               
-            }
-            else{
-        
-                commaActive.disabled = false; 
-                
-        
-            }
+function changePlusMinus(){
+    
+    let screen=document.getElementById('screen');
+
+    if(screen.value=='' ||screen.value==0 || screen.value=='0,'){
+ 
+        return;
+    }else{
+        if(screen.value[0]=='-'){
+
+            screen.value=screen.value.substring(1,screen.value.length);
+
+        }else{
+
+            screen.value=screen.value = '-' + screen.value;
         }
-    }
-    if(first_number.includes('x')){
-        y=parseFloat(first_number.substring(0,first_number.indexOf('x')));
-        commaActive.disabled = false
 
-        let arr=Array.from(document.getElementById('screen').value);
-        commas=0;
-
-        for(i=0;i<first_number.length;i++){
-            if(arr[i]==','){ commas++;}
-               console.log(commas);
-
-            if(commas>0){
-   
-               commaActive.disabled = true; 
-        
-               
-            }
-            else{
-        
-                commaActive.disabled = false; 
-        
-            }
-        }
-    }
-    if (first_number.includes('/')){
-        y=parseFloat(first_number.substring(0,first_number.indexOf('/')));
-        commaActive.disabled = false
-        console.log(first_number.indexOf('/')+1);
-
-     //   console.log(commas);
-            
-            if(x==',' && commas==0){
-               
-                commas++;
-               
-            }
-        
-            if(commas>0){
-                commaActive.disabled = true; 
-
-            }
-            else if(commas==0){
-                commaActive.disabled = false; 
-
-            }
-        
     }
 
+}
+
+function clickOperation(operator){
+
+
+    if(operation==''){
+        let screen=document.getElementById('screen');
+        operation=operator;
+    
+        first_number=screen.value;
+    
+        screen.value='0';  
+    }
+    else{
+       
+        calculate();
+        let screen=document.getElementById('screen');
+
+        first_number=screen.value;
+
+        operation=operator;
+
+        screen.value='0';  
+
+       
+    }
+     
 
     
-    if(document.getElementById('screen').value.includes('+')){
-        z=parseFloat(document.getElementById('screen').value.substring(document.getElementById('screen').value.indexOf('+')+1,document.getElementById('screen').value.length));
+}
+
+function calculate(){
+    
+    let screen=document.getElementById('screen');
+    let result;
+    let num1;
+    let num2='';
+    removeHighlighted();
+
+
+    second_number=screen.value;
+
+    num1=convertToNumber(first_number);
+    num2=convertToNumber(second_number);
+
+   
+    console.log(operation);
+    
+    if(operation=='+'){
+       
+        result=num1+num2;
+
 
     }
-    else if(document.getElementById('screen').value.includes('-')){
-        z=parseFloat(document.getElementById('screen').value.substring(document.getElementById('screen').value.indexOf('-')+1,document.getElementById('screen').value.length));
+    else if(operation=='-'){
+      
+        result=num1-num2;
 
     }
-    else if(document.getElementById('screen').value.includes('x')){
-        z=parseFloat(document.getElementById('screen').value.substring(document.getElementById('screen').value.indexOf('x')+1,document.getElementById('screen').value.length));
+    if(operation=='x'){
+       
+        result=num1*num2;
 
     }
-    else if(document.getElementById('screen').value.includes('/')){
-        z=parseFloat(document.getElementById('screen').value.substring(document.getElementById('screen').value.indexOf('/')+1,document.getElementById('screen').value.length));
+    if(operation=='/'){
+        
+        result=num1/num2;
 
     }
+    else if( num2=='' && operation==''){
+        screen=num1;
+    }
+    else if(num2==''){
+        screen.value=convertToString(result);
+        result='ERROR';
+    }
+
+    operation='';
+
+    screen.value=convertToString(result);
+    
+}
+
+function eraseValue(){
+    removeHighlighted();
+    let screen=document.getElementById('screen');
+    screen.value="0";    
+}
+
+function convertToNumber(text){
+
+    return parseFloat(text.replace(',','.'));
+}
+
+function convertToString(num){
+
+    if(num=='Infinity' || num=='-Infinity' || !(num==num)){
+        return "ERROR";
+    }
+
+    return num.toFixed(10-contZero(num)).toString().replace('.',',');
+}
+
+function contZero(num){
+    let zeros=0;
+    let cadena=num.toFixed(10).toString();
+   
+    if(!cadena.includes('.')){
+        return 0;
+    }
+
+    let point=cadena.length-1;
+
+    while(cadena[point]=='0'){
+        zeros++;
+        point--;
+    }
+
+    return zeros;
 }
 
 function changeHighlighted(changeClass){
@@ -175,125 +215,6 @@ function removeHighlighted(){
     
 }
 
-function eraseValue(x) {
-    removeHighlighted();
-    commas=0;
-    document.getElementById('screen').value = x;
-    document.getElementById('screen').placeholder = "0";
-
-}
-
-function calculate(){
-    removeHighlighted();
-    var number=document.getElementById('screen').value;
-    var number2=eval(number.replace('x','*'));
-    document.getElementById('screen').value=number2;
-
-    if(number2=='Infinity' || number2=='-Infinity' || !(number2==number2)){
-        document.getElementById('screen').value='ERROR';
-    }
-
-}
-
-function calculate2(){
-    commas=0;
-    let second_number=document.getElementById('screen').value;
-    second_number=second_number.replace(',','.');
-
-
-    if(second_number.includes('+')){
-        z=parseFloat(second_number.substring(second_number.indexOf('+')+1,second_number.length));
-        result=y+z;
-
-    }
-    else if(second_number.includes('-')){
-        z=parseFloat(second_number.substring(second_number.indexOf('-')+1,second_number.length));
-        result=y-z;
-
-    }
-    else if(second_number.includes('x')){
-        z=parseFloat(second_number.substring(second_number.indexOf('x')+1,second_number.length));
-        result=y*z;
-
-    }
-    else if(second_number.includes('/')){
-        z=parseFloat(second_number.substring(second_number.indexOf('/')+1,second_number.length));
-        result=y/z;
-
-    }
-    
-    result=parseFloat(result);
-    result=result.toString();
-    result=result.replace('.', ',');
-
-
-    if(second_number=='Infinity' || second_number=='-Infinity' || !(second_number==second_number) || result=='Infinity'){
-        document.getElementById('screen').value='ERROR';
-    }
-    else{
-        document.getElementById('screen').value=result;
-    }
-
-
-}
-
-function changePlusMinus(){
-
-    console.log(document.getElementById('screen').value.indexOf('-'));
-
-    if(document.getElementById('screen').value.includes('+') && ! isNaN(z)){
-         
-        document.getElementById('screen').value=y+"-"+z;
-    }
-    else if(document.getElementById('screen').value.includes('+')){
-        
-        document.getElementById('screen').value=y+"+";
-
-    }
-    else if(document.getElementById('screen').value.includes('-')  && ! isNaN(z)){
-
-        document.getElementById('screen').value=y+"+"+z;
-        console.log("hello");
-    }
-    else if(document.getElementById('screen').value.includes('-') && document.getElementById('screen').value.indexOf('-')!=0){
-        
-        document.getElementById('screen').value=y+"-";
-
-    }
-    else if(document.getElementById('screen').value.includes('x')   && ! isNaN(z)){
-        z=-z;
-
-        document.getElementById('screen').value=y+"x"+z;
-        console.log(z);
-
-    }
-    else if(document.getElementById('screen').value.includes('x')){
-
-        document.getElementById('screen').value=y+"x";
-    }
-
-    else if(document.getElementById('screen').value.includes('/')   && ! isNaN(z)){
-
-        z=-z;
-        document.getElementById('screen').value=y+"/"+z;
-        console.log(z);
-    }
-    else if(document.getElementById('screen').value.includes('/')){
-
-        document.getElementById('screen').value=y+"/";
-        
-    }
-    else{
-
-        document.getElementById('screen').value=-document.getElementById('screen').value;
-
-    }
-
-   
-
-      
-}
-
 function keyboard (TheEvent) { 
     events = TheEvent || window.event;
     k=events.keyCode; 
@@ -301,22 +222,22 @@ function keyboard (TheEvent) {
     if (k>47 && k<58) { 
        p=k-48; 
        p=String(p) 
-       returnValue(p); 
+       clickNumber(p); 
        }	
   
     if (k>95 && k<106) {
        p=k-96;
        p=String(p);
-       returnValue(p);
+       clickNumber(p);
        }
 
-    if (k==110 || k==190) {returnValue(",")} 
-    if (k==106 || k==88) {returnValue('x'); changeHighlighted(document.getElementById('multiply').classList)} 
-    if (k==107) {returnValue('+'); changeHighlighted(document.getElementById('plus').classList)} 
-    if (k==109) {returnValue('-'); changeHighlighted(document.getElementById('minus').classList)} 
-    if (k==111) {returnValue('/'); changeHighlighted(document.getElementById('division').classList)} 
-    if (k==32 || k==13) {calculate2()} //equal or space
-    if (k==27) {eraseValue(" ")} //C
+    if (k==110 || k==190) {clickComma()} 
+    if (k==106 || k==88) {clickOperation('x'); changeHighlighted(document.getElementById('multiply').classList)} 
+    if (k==107) {clickOperation('+'); changeHighlighted(document.getElementById('plus').classList)} 
+    if (k==109) {clickOperation('-'); changeHighlighted(document.getElementById('minus').classList)} 
+    if (k==111) {clickOperation('/'); changeHighlighted(document.getElementById('division').classList)} 
+    if (k==32 || k==13) {calculate()} //equal or space
+    if (k==27) {eraseValue()} //C
     if( k==17){ changePlusMinus()}
     
     }
