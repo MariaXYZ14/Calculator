2ecd4489688adf80cc9030542b8cd3399f2c2ef7
MariaXYZ14 <mariaxyz14@gmail.com>
2022-09-05 10:31:32 +0200

    automatic tests

--- steps.js
+++ steps.js
@@ -1,5 +1,5 @@
 const {Given,Then} = require('@cucumber/cucumber');
-
+const{expect} = require('@playwright/test'); //A space is required
 const url="http://127.0.0.1:5500/";
 
 Given('a user opens the app', async function () {
@@ -7,7 +7,8 @@ Given('a user opens the app', async function () {
     await page.goto(url);
   });
 
-Then('the display should show the following value:{string}', function (string) {      
+Then('the display should show the following value:{string}', async function (string) {      
     // Write code here that turns the phrase above into concrete actions
-    return 'pending';
+    const display = await page.locator('data-testid=display').innerText();
+    expect(display).toBe(string);
 });
