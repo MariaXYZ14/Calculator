2182fda16ad647648f55fa6ffaeba94c9ee7619e
MariaXYZ14 <mariaxyz14@gmail.com>
2022-09-05 11:04:31 +0200

    automatic tests

--- steps.js
+++ steps.js
@@ -9,6 +9,6 @@ Given('a user opens the app', async function () {
 
 Then('the display should show the following value:{string}', async function (string) {      
     // Write code here that turns the phrase above into concrete actions
-    const display = await page.locator('data-testid=display').innerText();
+    const display = await page.locator('data-testid=display').getAttribute('value');
     expect(display).toBe(string);
 });
