8a993ff72366a6588525afe10c233453230ca16a
MariaXYZ14 <mariaxyz14@gmail.com>
2022-09-07 12:42:14 +0200

    steps

--- steps/calculator-steps.js
+++ steps/calculator-steps.js
@@ -2,17 +2,66 @@ const {Given,When,Then} = require('@cucumber/cucumber');
 const{expect} = require('@playwright/test'); //A space is required
 const url="http://127.0.0.1:5500/";
 
-Given('a user opens the app', async function () {
+async function buttonClick(buttonId) {
+	await page.click(`[data-testid="${buttonId}"]`, { force: true });
+}
 
-    // Write code here that turns the phrase above into concrete actions
-    await page.goto(url);
-  });
+async function serialForEach(array, cb) {
+	return array.reduce((promise, data) => promise.then(() => cb(data)), Promise.resolve());
+}
 
-Then('the display should show the following value: {string}', async function (string) { 
+async function writeNumber(strNumber) {
+	let isNegative = false;
+	const digits = strNumber.split('');
+	await serialForEach(digits, async (digit) => {
+		if (digit === '-') {
+			isNegative = true;
+		} else {
+			await buttonClick(digit);
+		}
+	});
+	if (isNegative) {
+		await buttonClick('+-');
+	}
+}
 
-    // Write code here that turns the phrase above into concrete actions
-    // const display = await page.locator('data-testid=display').getAttribute('value');
-     const display = await page.locator('data-testid=display').inputValue();
-     expect(display).toBe(string);
+async function keyPress(keyId) {
+	await page.keyboard.press(keyId, { force: true });
+}
 
+Given('a user opens the app', async () => {
+	await page.goto(url);
+});
+
+Then('the display should show the following value: {string}', async (string) => {
+	//const display = await page.locator('data-testid=display').innerText();
+	const display = await page.locator('data-testid=display').inputValue();
+	expect(display).toBe(string);
+});
+
+Given('the display shows the following value: {string}', async (string) => {
+	await buttonClick('C');
+	await writeNumber(string);
+});
+
+When('the user presses the {string} button', async (string) => {
+	await buttonClick(string);
+});
+
+When('the user presses the {string} key', async (string) => {
+	await keyPress(string);
+});
+
+When('the user writes the number: {string}', async (string) => {
+	await writeNumber(string);
+});
+
+Then('the {string} button should be disabled', async (string) => {
+	const locator = page.locator(`[data-testid="${string}"]`);
+	await expect(locator).toBeDisabled();
+});
+
+Then('the {string} button should be enabled', async (string) => {
+	const locator = page.locator(`[data-testid="${string}"]`);
+	await expect(locator).toBeEnabled();
 });
