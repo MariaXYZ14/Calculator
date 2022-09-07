883bbdfd79c9c37c137fb2cca15d3a3e051a4c1c
MariaXYZ14 <mariaxyz14@gmail.com>
2022-09-07 12:15:41 +0200

    steps

--- steps/calculator-steps.js
+++ steps/calculator-steps.js
@@ -1,19 +1,18 @@
-const {Given,Then} = require('@cucumber/cucumber');
+const {Given,When,Then} = require('@cucumber/cucumber');
 const{expect} = require('@playwright/test'); //A space is required
-const url="https://mariaxyz14.github.io/Calculator/";
+const url="http://127.0.0.1:5500/";
 
 Given('a user opens the app', async function () {
 
     // Write code here that turns the phrase above into concrete actions
     await page.goto(url);
-
   });
 
-Then('the display should show the following value:{string}', async function (string) { 
+Then('the display should show the following value: {string}', async function (string) { 
 
     // Write code here that turns the phrase above into concrete actions
     // const display = await page.locator('data-testid=display').getAttribute('value');
-    const display = await page.locator('data-testid=display').inputValue();
-    expect(display).toBe(string);
-    
+     const display = await page.locator('data-testid=display').inputValue();
+     expect(display).toBe(string);
+
 });
